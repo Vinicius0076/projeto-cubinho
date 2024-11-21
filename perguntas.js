3c251b843e8f4525f9ec84dd9b8e6820525c591e
Vinicius0076 <viniciusmonteirorodrigues5@gmail.com>
2024-11-21 08:51:22 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'jogos digitais',
+    'Qual a espada mais fraca do minecraft,espada de ouro ou a de ferro?',
+    'a espada de ouro,porque a durabilidade é menor do que a de ferro.'
+)
+
+criaCartao(
+    'jogos digitais',
+    'poção de veneno no minecraft mata ou não?',
+    'Não,o personagem fica com um coração e meio de vida.'
+)
+
+criaCartao(
+    'jogos digitais',
+    'quantos mineiros são necessarios para fazer um set completo de armadura no minecraft?',
+    'São necessarios 24 mineiros para fazer um set completo.'
+)
+
+criaCartao(
+    'jogos digitais',
+    'Qual é o boss mais forte do minecraft?',
+    'O boss mais forte é o wither,porque ele é imune ao fogo,magia,queda,e sufocamento.'
+)
